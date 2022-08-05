c10e4827088863844608627b0b15e5ad50c6d215
Timmy-Codes <63141747+Timmy-Codes@users.noreply.github.com>
2022-08-05 13:39:04 +1000

    A slightly more succinct version of the code :)
    I am doing the intermediate C++ course, coming from coding for about a year in Java. And I gave this task much thought and came up with this logic that makes the code fairly short and contained. Focusing on all the lose scenarios, and then everything else is a win...or a tie which is taken care of in the first if().  Let me know what you think.

--- 3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS.cpp
+++ 3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS.cpp
@@ -1,99 +1,41 @@
 /*
-This program will allow you to play RPSLS against the computer.
+Rock Paper Scissors Lizard Spock
+(The Big Bang Theory)
 */
 
 #include <iostream>
-#include <cstdlib>
+#include <stdlib.h>
 
 int main() {
-
-  srand(time(NULL));
-  int computer = std::rand() % 5 + 1;
+  
+  srand (time(NULL));
+  int computer = rand() % 5 + 1; //random number between 1-3 inclusive
   int user = 0;
   
-  std::cout << "=================================\n";
-  std::cout << "rock paper scissors lizard spock!\n";
-  std::cout << "=================================\n";
+  // prompt user for input
+  std::cout << "====================\n";
+  std::cout << "Rock Paper Scissors!\n";
+  std::cout << "====================\n";
 
   std::cout << "1) ✊\n";
   std::cout << "2) ✋\n";
   std::cout << "3) ✌️\n";
-  std::cout << "4) lizard\n";
-  std::cout << "5) spock\n\n";
+  std::cout << "4) 🖖\n";
+  std::cout << "5) 🦎\n";
 
-  std::cout << "shoot: ";
-  
-  int rock = 1;
-  int paper = 2;
-  int scissors = 3;
-  int lizard = 4;
-  int spock = 5;
-  
+  std::cout <<  "Shoot! ";
   std::cin >> user;
+
+  std::cout << "Computer chose: " << computer << std::endl;
   
-  /* 
-  rock > scissors
-  scissors > paper
-  paper > rock
-  lizard > spock
-  spock > scissors
-  scissors > lizard
-  lizard > paper
-  paper > spock
-  spock > rock
-  rock > lizard
-  */
-  
-  std::cout << "computer: " << computer << "\n\n";
-  
-  if (user == rock && computer == scissors) {
-      
-    std::cout << "You Win!\n";
-    
-  } else if (user == scissors && computer == paper) {
-    
-    std::cout << "You Win!\n";
-    
-  } else if (user == paper && computer == rock) {
-    
-    std::cout << "You Win!\n";
-      
-  } else if (user == lizard && computer == spock) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == spock && computer == scissors) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == scissors && computer == lizard) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == lizard && computer == paper) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == paper && computer == spock) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == spock && computer == rock) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == rock && computer == lizard) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == computer) {
-      
-    std::cout << "Tie!\n";
-      
-  } else {
-      
-    std::cout << "You Lose!\n";
-      
+  if (user == computer){
+    std::cout << "It's a Tie!!" << std::endl;
   }
-  
+  else if (computer == user + 1 || computer == user+3 || computer == user-2 || computer == user/5){
+    std::cout << "You Lose!" << std::endl;
+  }
+  else{
+    std::cout << "You Win!" << std::endl;
+  }
+  return 0;
 }
